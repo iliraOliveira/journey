bb53728906dd5cbee2e6bbb8e1eff7981b975485
Ivan Lira de Oliveira <ivan.lira@pixeon.com>
2024-07-09 21:48:01 -0300

    Adicionar arquivos de projeto.

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,19 @@
+﻿using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson tripJson)
+        {
+            var useCase = new RegisterTripUseCase();
+            useCase.Execute();
+            return Created();
+        }
+    }
+}
---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,35 @@
+﻿using Journey.Communication.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripUseCase
+    {
+        public void Execute(RequestRegisterTripJson tripJson)
+        {
+            validate(tripJson);
+        }
+
+        private void validate(RequestRegisterTripJson tripJson)
+        {
+            if(string.IsNullOrWhiteSpace(tripJson.Name))
+            {
+                throw new ArgumentException("Nome não pode ser vazio.");
+            }
+
+            if(tripJson.StartDate < DateTime.UtcNow)
+            {
+                throw new ArgumentException("A viagem não pode ser registrada para uma data passada.");
+            }
+
+            if (tripJson.EndDate >= DateTime.UtcNow)
+            {
+                throw new ArgumentException("A viagem não pode ser registrada para uma data passada.");
+            }
+        }
+    }
+}
